0f7411c920145d8124c4d84f86104b68b74d6ff1
MilesCranmer <miles.cranmer@gmail.com>
2023-12-28 09:21:11 +0000

    Formatting

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,20 @@
 using UniAdminTools
 using Documenter
 
-DocMeta.setdocmeta!(UniAdminTools, :DocTestSetup, :(using UniAdminTools); recursive=true)
+DocMeta.setdocmeta!(UniAdminTools, :DocTestSetup, :(using UniAdminTools); recursive = true)
 
 makedocs(;
-    modules=[UniAdminTools],
-    authors="MilesCranmer <miles.cranmer@gmail.com> and contributors",
-    repo="https://github.com/MilesCranmer/UniAdminTools.jl/blob/{commit}{path}#{line}",
-    sitename="UniAdminTools.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://MilesCranmer.github.io/UniAdminTools.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [UniAdminTools],
+    authors = "MilesCranmer <miles.cranmer@gmail.com> and contributors",
+    repo = "https://github.com/MilesCranmer/UniAdminTools.jl/blob/{commit}{path}#{line}",
+    sitename = "UniAdminTools.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://MilesCranmer.github.io/UniAdminTools.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/MilesCranmer/UniAdminTools.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/MilesCranmer/UniAdminTools.jl", devbranch = "main")
